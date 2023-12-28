02ec11d87f189dbb310ffa7378d136a1541dc98f
Skyler Kinney <Skylerjkinney@gmail.com>
2023-12-28 14:19:43 -0700

    imgs sorted

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,16 +1,15 @@
 <template>
-  <img
-    src="https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?q=80&w=2676&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    alt="tree">
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
-        class="rounded-circle">
+  <main class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+    <div class="home-card  p-5 card align-items-center shadow rounded elevation-3">
+      <img class="bg-img" src="https://em-content.zobj.net/source/twitter/348/mushroom_1f344.png" alt="tree">
       <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
+        Vue miner
       </h1>
+      <button>pick</button>
+      <button>picker</button>
+      <button>maxpick</button>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
@@ -24,14 +23,6 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.bgImg {
-  height: 200px;
-  max-width: 200px;
-  width: 100%;
-  object-fit: contain;
-  object-position: center;
-}
-
 .home {
   display: grid;
   height: 80vh;
@@ -40,14 +31,19 @@ export default {
   user-select: none;
 
   .home-card {
-    width: clamp(500px, 50vw, 100%);
+    width: clamp(500px, 60vw, 100%);
+    background-image: url('https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmF2ZXxlbnwwfHwwfHx8MA%3D%3D');
+    background: cover;
+    background-size: cover;
+    background-repeat: no-repeat;
 
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
+    >.bg-img {
+      height: 50%;
+      width: 50%;
       object-fit: contain;
-      object-position: center;
+      object-position: bottom;
     }
+
   }
-}</style>
+}
+</style>
